c12f3db453b8bd5ee2aba007434d58a91d616d2e
Sareva00 <sareva00@hotmail.com>
2022-09-06 10:45:39 +0200

    corrctions ng

--- ngfunction.py
+++ ngfunction.py
@@ -1,5 +1,4 @@
-%matplotlib inline
-%config InlineBackend.figure_format = 'retina'
+
 import camb
 from camb import model, initialpower
 from camb.sources import GaussianSourceWindow, SplinedSourceWindow
@@ -14,15 +13,10 @@ from matplotlib import pyplot as plt
 z = np.arange(0.7,2.1,0.1)
 n = np.array([17.5,19,18,16,15,13.2,12,10,7.5,5.5,3.5,3.2,2,1])
 
-it, ax = plt.subplots()
-
-ax.set_xlabel('z')
-ax.set_ylabel('n*10^4 [h^3/Mpc^3]')
-
-ax.plot(z, n, color='g', ls ='-')  #aggiungi gli errori
-
 ng =  interpolate.interp1d(z,n,kind='cubic')
-plt.plot(z, ng(z), '--')
 
-def Ng (z):
-    return ng(z)
+def Ng(x):
+    ng =  interpolate.interp1d(z,n,kind='cubic')
+    y = float(ng(x))
+    print(y)
+    return y
