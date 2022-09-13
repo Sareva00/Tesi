a8b97b1ce60c6a87dc98dfcd0fd2c46c44a9e093
Sareva00 <sareva00@hotmail.com>
2022-09-13 13:46:54 +0200

    modifiche

--- ngfunction.py
+++ ngfunction.py
@@ -10,13 +10,16 @@ from scipy import optimize
 import matplotlib
 from matplotlib import pyplot as plt
 
-z = np.arange(0.7,2.1,0.1)
-n = np.array([17.5,19,18,16,15,13.2,12,10,7.5,5.5,3.5,3.2,2,1])
-
-ng =  interpolate.interp1d(z,n,kind='cubic')
 
 def Ng(x):
+
+    z = np.arange(0.7,2.1,0.1)
+    n = np.array([17.5,19,18,16,15,13.2,12,10,7.5,5.5,3.5,3.2,2,1])
+    for i in range (len(z)):
+        z[i]= (round(z[i],1))
     ng =  interpolate.interp1d(z,n,kind='cubic')
+    
+       
+
     y = float(ng(x))
-    print(y)
     return y
